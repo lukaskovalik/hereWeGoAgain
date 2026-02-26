7e64e6fe6167251042ea9573ef2f3c4ca0691cbb
lukaskovalik <128225231+lukaskovalik@users.noreply.github.com>
2026-02-26 09:24:44 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
+        mapaPrint();
+    }
+    static void mapaPrint () {
         String mapa = "    #####              #   #              #$  #            ###  $##           #  $ $ #         ### # ## #   #######   # ## #####  ..## $  $          ..###### ### #@##  ..#    #     #########    #######        ";
         int col = 11;
         int row = 19;
